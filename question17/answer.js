c02f3a86b2f763b8e2dd7651ede92eac209dbe10
luckyzi <luckyzi1109@gmail.com>
2022-02-11 10:13:46 +0900

    question 17-18

---
+++ question17/answer.js
@@ -0,0 +1,6 @@
+let num = prompt();
+console.log(num >= 150 ? "YES" : "NO");
+
+// 단순한 if 문제라 3항연산자로 풀어봄
+
+// 조건1 ? true일 때 결과 : flase일 때 결과
---
+++ question18/answer.js
@@ -0,0 +1,13 @@
+let str = prompt().split(" ");
+// 입력 받을 때 공백에 따라 배열로 나눠서 받기
+
+let result = 0;
+// 결과를 담을 변수
+
+for (let i = 0; i < str.length; i++) {
+  result += Number(str[i]);
+  // 반복할 때마다 배열 내의 원소들을 합함
+}
+
+console.log(result / str.length);
+// 평균값 출력
