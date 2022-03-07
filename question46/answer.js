9121ffb3db29acac4181ab3d2dd6d31768dfea6a
luckyzi <luckyzi1109@gmail.com>
2022-03-08 00:00:40 +0900

    fixed question46

--- question46/answer.js
+++ question46/answer.js
@@ -1,14 +1,14 @@
-or(let i = 0 ; i < 20; i++){
-  let stringValue = (i+1).toString()
-  let result = 0
-  for(let j = 0; j < stringValue.length; j++){
-    if(stringValue[j] === '1'){
-      result += 1
-    }else if(stringValue[j] === '2'){
-      result += 2
-    }else{
-      result += i
+for (let i = 0; i < 20; i++) {
+  let stringValue = (i + 1).toString();
+  let result = 0;
+  for (let j = 0; j < stringValue.length; j++) {
+    if (stringValue[j] === "1") {
+      result += 1;
+    } else if (stringValue[j] === "2") {
+      result += 2;
+    } else {
+      result += i;
     }
   }
-  console.log(result)
+  console.log(result);
 }
